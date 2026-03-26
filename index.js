2108b4e6bd3f4b8bf81c4e52205e6b43cc387d3c
faridbatilada20-hash <faridbatilada20@gmail.com>
2026-03-26 20:26:14 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion
+} = require("@whiskeysockets/baileys")
+
+const readline = require("readline")
+const { handleMessage } = require("./src/handler")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function question(text) {
+  return new Promise(resolve => rl.question(text, resolve))
+}
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("session")
+  const { version } = await fetchLatestBaileysVersion()
+
+  const sock = makeWASocket({
+    auth: state,
+    version
+  })
+
+  sock.ev.on("creds.update", saveCreds)
+
+  // 👉 kalau belum login
+  if (!sock.authState.creds.registered) {
+
+    let nomor = await question("Masukkan nomor WhatsApp (contoh: 628xxx): ")
+
+    nomor = nomor.replace(/[^0-9]/g, "") // bersihin simbol
+
+    const code = await sock.requestPairingCode(nomor)
+
+    console.log("\n🔥 Pairing Code Kamu:")
+    console.log(code)
+    console.log("Masukkan ke WhatsApp > Perangkat Tertaut\n")
+
+    rl.close()
+  }
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const msg = messages[0]
+    if (!msg.message) return
+
+    handleMessage(sock, msg)
+  })
+}
+
+startBot()
